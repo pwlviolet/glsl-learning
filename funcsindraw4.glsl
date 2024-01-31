a7dc70c87a7db34b7149c081bede2101859703bd
wl_p <1169664352@qq.com>
2024-01-31 22:39:27 +0800

    draw function

--- funcsindraw4.glsl
+++ funcsindraw4.glsl
@@ -1,3 +1,8 @@
+#define PI 3.141592653
+vec2 fixuv(vec2 c)
+{
+    return 3.*(c-.5*iResolution.xy)/min(iResolution.x,iResolution.y)*2.;
+}
 vec3 Grid(vec2 uv)
 {
     vec3 col;
@@ -22,37 +27,43 @@ vec3 Grid(vec2 uv)
     return col;
 }
 
-vec2 fixuv(vec2 c)
+
+float drawsegment(in vec2 uv,in vec2 start,in vec2 end,in float width)
 {
-    return 3.*(c-.5*iResolution.xy)/min(iResolution.x,iResolution.y)*2.;
-}
-
-
-
-vec3 drawsegment(vec2 uv,vec2 start,vec2 end,float width)
-{
+    float f=0.;
     vec2 a=start,b=end,p=uv;
     vec2 ap=p-a;
     vec2 ab=b-a;
     float proj=clamp(dot(ap,ab)/dot(ab,ab),0.0,1.0);
     float d=length(proj*ab-ap);
     if(d<width)
-    return vec3(1.0);
+    {
+        f=1.0;
+    }
+    return f;
 
 }
 
 float func(in float x)
+{   float T=4.0+2.0*sin(iTime);
+    return sin(2.0*PI/T*x);
+}
+
+float drawfunc(in vec2 uv)
 {
-    return sin(x);
+    float f=0.0;
+    for(float x=0.;x<=iResolution.x;x+=1.0)
+    {
+        float fx=fixuv(vec2(x,0.0)).x;
+        float fxe=fixuv(vec2(x+1.0,0.0)).x;
+        f+=drawsegment(uv,vec2(fx,func(fx)),vec2(fxe,func(fxe)),fwidth(uv.x));
+    }
+    return clamp(f,0.0,1.0);
 }
 void mainImage(out vec4 fragColor,in vec2 fragCoord)
 {
     vec2 uv=fixuv(fragCoord);
     vec3 col=Grid(uv);
-    vec2 start=vec2(0.0);
-    vec2 end=vec2(2.0,2.0);
-    float width=0.01;
-    vec3 line=drawsegment(uv,start,end,width);
-    col=col+line;
-    fragColor=vec4(col,1.);
+    vec3 color=mix(col,vec3(1.0,1.0,0.0),drawfunc(uv));
+    fragColor=vec4(color,1.);
 }
