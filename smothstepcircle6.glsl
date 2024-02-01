d2b41d107d4a78a62267441da33d9dd7ceceea5d
wl_p <1169664352@qq.com>
2024-02-01 22:14:45 +0800

    feat:smoothstep

--- smothstepcircle6.glsl
+++ smothstepcircle6.glsl
@@ -6,24 +6,7 @@ vec2 fixuv(vec2 c)
 vec3 Grid(vec2 uv)
 {
     vec3 col;
-    // vec2 cell=fract(uv);
     vec2 cell=1.0- 2.0*abs(fract(uv)-0.5);
-    // if(cell.x<2.0*fwidth(uv.x))
-    // {
-    //     col=vec3(1.,1.,1.);
-    // }
-    // if(cell.y<2.0*fwidth(uv.y))
-    // {
-    //     col=vec3(1.,1.,1.);
-    // }
-    // if(abs(uv.y)<=fwidth(uv.y))
-    // {
-    //     col=vec3(1.,0.,0.);
-    // }
-    // if(abs(uv.x)<=fwidth(uv.x))
-    // {
-    //     col=vec3(0.,1.,0.);
-    // }
     col=vec3(smoothstep(2.0*fwidth(uv.x),1.99*fwidth(uv.x),cell.x));
     col+=vec3(smoothstep(2.0*fwidth(uv.y),1.99*fwidth(uv.y),cell.y));
     col.bg*=1.0-smoothstep(fwidth(uv.y),0.99*fwidth(uv.y),abs(uv.y));
@@ -41,10 +24,7 @@ float drawsegment(in vec2 uv,in vec2 start,in vec2 end,in float width)
     float proj=clamp(dot(ap,ab)/dot(ab,ab),0.0,1.0);
     float d=length(proj*ab-ap);
     f=smoothstep(width,0.99*width,d);
-    // if(d<width)
-    // {
-    //     f=1.0;
-    // }
+
     return f;
     
 
@@ -71,7 +51,9 @@ float drawfunc(in vec2 uv)
 void mainImage(out vec4 fragColor,in vec2 fragCoord)
 {
     vec2 uv=fixuv(fragCoord);
-    vec3 col=Grid(uv);
-    vec3 color=mix(col,vec3(1.0,1.0,0.0),drawfunc(uv));
-    fragColor=vec4(color,1.);
+    // vec3 col=Grid(uv);
+    // vec3 color=mix(col,vec3(1.0,1.0,0.0),drawfunc(uv));
+    float c=length(uv);
+     c=1.0-smoothstep(0.99,1.0,c);
+    fragColor=vec4(vec3(c),1.);
 }
