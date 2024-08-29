3103c1b4cc7708dbe01f72c4d6e19c74b25340e5
wl_p <1169664352@qq.com>
2024-08-29 23:49:17 +0800

    feat:texture AA

--- 1.初始uv.glsl
+++ 1.初始uv.glsl
@@ -3,6 +3,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	vec2 uv=(2.0*fragCoord-iResolution.xy)/min(iResolution.x,iResolution.y);
 	float c=length(uv);
 	fragColor=vec4(vec2(c),0.0,1.0);
+	// fragColor=vec4(fragCoord.y/iResolution.y,0.,0.,1.0);
 }
 
 
